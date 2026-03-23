f30a68e661a0487de715f8f9a7429987c2a5b3fc
albertliu <xiaochuan_liu@hotmail.com>
2026-03-23 08:06:30 +0800

    redo

--- rptPayInvoiceIncDetail.js
+++ rptPayInvoiceIncDetail.js
@@ -6,15 +6,15 @@
 								&nbsp;开票日期&nbsp;<input id="rptPayInvoiceStartDate1" class="easyui-datebox" data-options="height:22,width:100" />-<input id="rptPayInvoiceEndDate1" class="easyui-datebox" data-options="height:22,width:100" />
 								&nbsp;&nbsp;<input class="easyui-checkbox" id="rptPayInvoiceAutoPay" name="rptPayInvoiceAutoPay" value="1"/>&nbsp;自助收费&nbsp;
 								<input class="easyui-checkbox" id="rptPayInvoiceAutoInvoice" name="rptPayInvoiceAutoInvoice" value="1"/>&nbsp;自助开票&nbsp;
-								<input class="easyui-checkbox" id="rptPayInvoiceReceivable" name="rptPayInvoiceReceivable" value="1"/>&nbsp;应收款&nbsp;
-								<input class="easyui-checkbox" id="rptPayInvoiceReceived" name="rptPayInvoiceReceived" value="1"/>&nbsp;已收款&nbsp;
 							</span>
 							<span id="rptPayInvoiceItem1">
+								<input class="easyui-checkbox" id="rptPayInvoiceReceivable" name="rptPayInvoiceReceivable" value="1"/>&nbsp;应收款&nbsp;
+								<input class="easyui-checkbox" id="rptPayInvoiceReceived" name="rptPayInvoiceReceived" value="1"/>&nbsp;已收款&nbsp;
 								<input class="button" type="button" id="btnRptPayInvoiceReceive" value="应收确认" />&nbsp;
-								<a class="easyui-linkbutton" id="btnRptPayInvoice" href="javascript:void(0)"></a>
-								<a class="easyui-linkbutton" id="btnRptPayInvoiceDownLoad" href="javascript:void(0)"></a>
 							</span>
 							<span style="padding-left:50px;">
+								<a class="easyui-linkbutton" id="btnRptPayInvoice" href="javascript:void(0)"></a>
+								<a class="easyui-linkbutton" id="btnRptPayInvoiceDownLoad" href="javascript:void(0)"></a>
 							</span>
 				        </form>
 					</div>
